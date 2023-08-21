85b46e48e2c0976ae52c059e3df5834d4b480526
kalpanakale1 <aarti.ssingh707@gmail>
2023-08-21 09:55:53 +0530

    passport local

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = require("./Routes/Students.route");
 const connect = require("./config/db");
 const cors = require("cors");
-const cookiesparser=require("cookie-parser")
-const session=require("express-session")
+const cookiesparser = require("cookie-parser");
+const session = require("express-session");
+const passport = require("passport");
+const localization = require("./middlewares/LocalAuth");
+
 const app = express();
-app.use(cookiesparser())
-app.use(session({secret:"secret-key"}));
+app.use(cookiesparser());
+app.use(session({ secret: "secret-key" }));
+localization(passport)
+app.use(passport.initialize());
+app.use(passport.session());
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.set("views", __dirname + "/views");
